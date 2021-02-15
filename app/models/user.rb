b34a3f2ca37fffd975d96ba8131aea8fc1681611
tsurukame1989 <72078969+tsurukame1989@users.noreply.github.com>
2021-02-15 19:39:41 +0900

    Userモデルの可読性向上
    with_optionsを使ってコードを簡略化

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,23 +7,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-         with_options presence: true do
-          # ニックネームは必須である事
-          validates :nickname
-          # emailは必須、一意性で、＠を含まなければいけない事
-          validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "is invalid. Input '@'." }
-          # passwordは必須、６文字以上で、半角英数字の混合である事
-          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])\w{6,100}\z/, message: "is invalid. Input half-width both characters & numbers." }
-          # 名前は必須である事
-          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
-          # 苗字は必須である事
-          validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
-          # 名前の読み仮名は必須で、全角カタカナである事
-          validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
-          # 苗字の読み仮名は必須で、全角カタカナである事
-          validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
-          # 誕生日は必須である事
-          validates :birthday
-         end
+
+  with_options presence: true do
+    # ニックネームは必須である事
+    validates :nickname
+    # emailは必須、一意性で、＠を含まなければいけない事
+    validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "is invalid. Input '@'." }
+    # passwordは必須、６文字以上で、半角英数字の混合である事
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])\w{6,100}\z/, message: "is invalid. Input half-width both characters & numbers." }
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." } do
+      # 名前は必須で,全角ひらがな、カタカナ、漢字である事
+      validates :first_name
+      # 苗字は必須で,全角ひらがな、カタカナ、漢字である事
+      validates :last_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." } do
+      # 名前の読み仮名は必須で、全角カタカナである事
+      validates :first_name_reading
+      # 苗字の読み仮名は必須で、全角カタカナである事
+      validates :last_name_reading
+    end
+
+    # 誕生日は必須である事
+    validates :birthday
+  end
 end
