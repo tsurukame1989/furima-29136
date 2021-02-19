1c8b8a4cd85d86b39675ac61d72d282e94cf0129
tsurukame1989 <72078969+tsurukame1989@users.noreply.github.com>
2021-02-19 19:49:09 +0900

    Itemモデルのバリデーション修正
    ","忘れ

--- app/models/item.rb
+++ app/models/item.rb
@@ -30,7 +30,7 @@ class Item < ApplicationRecord
       # 価格についての情報が必須であること
       validates :price, 
                 # 価格の範囲が¥300~¥9,999,999の間であること
-                numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to:9999999, message: "is invalid. Input 300 ~ 9999999 numbers." }
+                numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to:9999999, message: "is invalid. Input 300 ~ 9999999 numbers." },
                 # 販売価格は半角数字のみ保存可能であること
                 format: { with: /\A[0-9]+\z/, message: "is invalid. Input half_with numbers." }
     end
