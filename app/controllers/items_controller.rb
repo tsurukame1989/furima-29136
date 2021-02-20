fb84bdfe86f0a8eeb0abd85db20b636fb23967a5
tsurukame1989 <72078969+tsurukame1989@users.noreply.github.com>
2021-02-20 12:12:44 +0900

    商品出品機能を実装（画像登録）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params 
-    params.require(:item).permit(:name, :info, :category_id, :condition_id, :delivery_fee_id, :delivery_source_id, :delivery_days_id, :price ).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :info, :category_id, :condition_id, :delivery_fee_id, :delivery_source_id, :delivery_days_id, :price ).merge(user_id: current_user.id)
   end
 end
 
