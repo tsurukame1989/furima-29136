6d4d328e4f13e9d62e76e5e2077e581d708d2bf8
tsurukame1989 <72078969+tsurukame1989@users.noreply.github.com>
2021-02-20 21:06:51 +0900

    自動変換の表示を３桁毎に区切る機能の実装

--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -4,7 +4,7 @@ function priceCalculation() {
   const profit = document.getElementById("profit");
 
   //手数料倍率10%を設定し、taxに代入
-  const tax = 0.1
+  const tax = 0.1;
 
   itemPrice.addEventListener("input", () => {
     // 価格フォームに入力した商品価格をinputPriceに代入
@@ -13,12 +13,12 @@ function priceCalculation() {
     // inputPrice * taxの計算をして、小数点を切り捨てた値をtaxPriceに代入
     const taxPrice = Math.floor(inputPrice * tax);
 
-    // "id:add-tax-price"の値をtaxPriceの値に置換
-    addTaxPrice.innerHTML = taxPrice
+    // "id:add-tax-price"の値を3桁毎に区切ってtaxPriceの値に置換
+    addTaxPrice.innerHTML = taxPrice.toLocaleString();
     
-    // "id:profit"の値を(inputPrice - taxPrice)に置換
-    profit.innerHTML = (inputPrice - taxPrice)
+    // "id:profit"の値を3桁毎に区切って(inputPrice - taxPrice)に置換
+    profit.innerHTML = (inputPrice - taxPrice).toLocaleString();
   })
 }
 
-window.addEventListener('load', priceCalculation)
+window.addEventListener('load', priceCalculation);
