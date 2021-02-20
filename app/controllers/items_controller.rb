2888d493f46637bc4d1f6621403c5e45e4a7445f
tsurukame1989 <72078969+tsurukame1989@users.noreply.github.com>
2021-02-20 10:37:54 +0900

    商品出品機能を実装（画像登録以外）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
+  def create
+    @item = Item.create(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to action: :index
+    else
+      render  action: :new
+    end
+  end
+
+  private
+  def item_params 
+    params.require(:item).permit(:name, :info, :category_id, :condition_id, :delivery_fee_id, :delivery_source_id, :delivery_days_id, :price ).merge(user_id: current_user.id)
+  end
 end
+
