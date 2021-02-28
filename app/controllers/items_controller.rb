668bd9d595bca5f94a0ce3c667a27952ac61c047
tsurukame1989 <72078969+tsurukame1989@users.noreply.github.com>
2021-02-28 11:17:18 +0900

    商品編集画面の作成
    ・コントローラーで@editのインスタンス定義
    ・商品編集ページをfurima素材集からコピペ
    ・商品編集ページへのリンクを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,6 +28,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params 
     params.require(:item).permit(:image, :name, :info, :category_id, :condition_id, :delivery_fee_id, :delivery_source_id, :delivery_days_id, :price ).merge(user_id: current_user.id)
