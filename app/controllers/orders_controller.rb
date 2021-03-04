00ad82ab2f242bf96d89ada60242f9fe01fdbffb
tsurukame1989 <72078969+tsurukame1989@users.noreply.github.com>
2021-03-04 18:48:55 +0900

    Formオブジェクトに対応する様、Orderコントローラーを編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,5 +4,20 @@ class OrdersController < ApplicationController
     @price = @item.price.to_s(:delimited)
   end
 
+  def create
+    @order_shipping = OrderShipping.create(order_shipping)
+    if @order_shipping.valid?
+      @order_shipping.save
+      redirect_to action: :index
+    else
+      redirect_to item_orders_path(@item.id)
+    end
+  end
+
   private
+
+  def order_shipping
+    binding.pry
+    params.require(:order_shipping).permit(:token, :postal_code, :delivery_source_id, :city, :house_number, :building_name, :phone_number)
+  end
 end
