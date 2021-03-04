cadff3a0fe8ac2242f3bca5bc0bf499c4fdf98fd
tsurukame1989 <72078969+tsurukame1989@users.noreply.github.com>
2021-03-04 23:03:20 +0900

    Formオブジェクト、コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,19 +5,19 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order_shipping = OrderShipping.create(order_shipping)
+    @item = Item.find(params[:item_id])
+    @order_shipping = OrderShipping.new(order_shipping_params)
     if @order_shipping.valid?
       @order_shipping.save
-      redirect_to action: :index
+      return redirect_to root_path
     else
-      redirect_to item_orders_path(@item.id)
+      redirect_to item_orders_path(@item.id), flash: { error: @order_shipping.errors.full_messages }
     end
   end
 
   private
 
-  def order_shipping
-    binding.pry
-    params.require(:order_shipping).permit(:token, :postal_code, :delivery_source_id, :city, :house_number, :building_name, :phone_number)
+  def order_shipping_params
+    params.permit(:postal_code, :delivery_source_id, :city, :house_number, :building_name, :phone_number, :item_id, :user_id).merge(user_id: current_user.id)
   end
 end
