7f7af8f4f961568f9fa256137b2140f1870e459b
tsurukame1989 <72078969+tsurukame1989@users.noreply.github.com>
2021-03-04 23:21:08 +0900

    消し忘れていたbinding.pry削除

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -19,7 +19,6 @@ class OrderShipping
   end
 
   def save
-    binding.pry
     order = Order.create(user_id:user_id , item_id: item_id)
     ShippingAddress.create(postal_code: postal_code, delivery_source_id: delivery_source_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
